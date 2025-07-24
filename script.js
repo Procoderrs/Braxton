913319a8468cb6e590d741e0ccc4be998e496dfb
Zeenat <zeenat.zeni0112@gmail.com>
2025-07-24 17:53:13 +0500

    code

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+setTimeout(() => {
+    let preAnimationDiv=document.getElementById('pre').classList.add('hidden')
+    let main_content=document.getElementById('main-content').classList.remove('hidden')
+}, 1000);
+})
+
+
+/* checking dark and light theme */
+
+ 
