56830423addc6b426b00b2b9855c7b3fa445c17d
Zeenat <zeenat.zeni0112@gmail.com>
2025-07-25 18:25:46 +0500

    added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded',function(){
 setTimeout(() => {
     let preAnimationDiv=document.getElementById('pre').classList.add('hidden')
     let main_content=document.getElementById('main-content').classList.remove('hidden')
-}, 1000);
+}, 500);
 })
 
 
