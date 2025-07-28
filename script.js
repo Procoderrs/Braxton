9615320479acc58141f7acd109a46e0fba6477a5
Zeenat <zeenat.zeni0112@gmail.com>
2025-07-28 19:07:26 +0500

    code-resume-portion

--- script.js
+++ script.js
@@ -71,8 +71,89 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }, {
-    threshold: 0.1 // section should be 40% visible
+    threshold: 0 // section should be 40% visible
   });
 
   sections.forEach(section => observer.observe(section));
 });
+
+ const feedbacks = [
+      {
+        img: "./src/images/400x400_t01.webp",
+        name: "Alex Tomato",
+        pre:'Brand Manager in',
+        role: " instant Design",
+        
+        message: "lorem ipsum dollar emit lorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emit lorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emit",
+      },
+      {
+        img: "./src/images/400x400_t02.webp",
+        name: "Jenny Pineapple",
+        pre:'SEO in',
+        role: " creative people",
+       
+        message: "lorem ipsum dollar emit lorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emit lorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emitlorem ipsum dollar emit",
+      },
+    ];
+
+    const wrapper = document.getElementById("feedbackWrapper");
+
+    feedbacks.forEach((item,index)=>{
+      const slide=document.createElement('div')
+      slide.className=`feedback-slide ${index===0?"active":""}`;
+      slide.innerHTML=`
+      <div class="p-8 bg-[#e6ebf5] dark:bg-black">
+      <div class="flex gap-4 items-center mb-4">
+      <img src='${item.img}' alt="${item.name}" class=" w-32 h-32 rounded-2xl" />
+      <div class=" flex flex-col">  
+      <p class="font-semibold text-[26px]">${item.name} </p> 
+      <p class="text-sm text-gray-500"> ${item.role} </p>
+      <div class=" flex items-start  gap-1">
+      <i class="ri-star-s-line text-purple-500 w-3 h-3"></i>
+      <i class="ri-star-s-line text-purple-500 w-3 h-3"></i>
+      <i class="ri-star-s-line text-purple-500 w-3 h-3"></i>
+      <i class="ri-star-s-line text-purple-500 w-3 h-3"></i>
+      <i class="ri-star-s-line text-purple-500 w-3 h-3"></i>
+      </div>
+      </div>
+      </div>
+      <p class=" text-gray-700 "> ${item.message} </p>
+      `;
+      wrapper.appendChild(slide)
+    });
+    let currentIndex=0;
+    const slides=document.querySelectorAll('.feedback-slide')
+
+
+    function showSlide(index){
+      slides.forEach ((slidee,i)=>{
+     slidee.classList.toggle('active',i===index);
+      })
+    }
+
+    function nextSlide(){
+      currentIndex=(currentIndex+1)%slides.length;
+      showSlide(currentIndex);
+    }
+    function prevSlide(){
+      currentIndex=(currentIndex -1 +slides.length)% slides.length;
+      showSlide(currentIndex);
+    }
+    
+    document.getElementById('nextBtn').addEventListener('click',function(){
+      nextSlide();
+      resetAutoplay();
+    })
+
+    document.getElementById('prevBtn').addEventListener('click',function(){
+      prevSlide();
+      resetAutoplay()
+    })
+    let autoPlay=setInterval(nextSlide,2000);
+
+    function resetAutoplay(){
+      clearInterval(autoPlay);
+      autoPlay=setInterval(nextSlide,2000)
+    };
+    
+  
